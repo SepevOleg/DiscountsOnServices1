5436c4b226a97015d2a0de031d9f7f1668d5b7ab
SepevOleg <107648948+SepevOleg@users.noreply.github.com>
2022-07-09 11:03:25 +0300

    media+js

--- js/header.js
+++ js/header.js
@@ -25,3 +25,30 @@ function submenuLanguageWrap() {
     })
 }
 submenuLanguageWrap()
+
+// media burger 
+
+const hamb = document.querySelector("#hamb");
+const popup = document.querySelector("#popup");
+const menu = document.querySelector("#menu").cloneNode(1);
+const btn_login = document.querySelector("#btn_log")
+const body = document.body;
+
+hamb.addEventListener("click", hambHandler)
+
+function hambHandler(e){
+  e.preventDefault();
+  popup.classList.toggle("open");
+  hamb.classList.toggle("active")
+  body.classList.toggle("noscroll")
+  
+  renderPopup();
+}
+
+function renderPopup() {
+  popup.appendChild(menu);
+}
+
+function renderBtnlogin() {
+  btnLogin.appendChild(btn_log)
+}
